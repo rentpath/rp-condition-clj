b4745c436b681eb2d84228aecc18e09a6ebd6095
Jenkins Continous Integration <jenkins@jenkins-slave-02.ci.atl.primedia.com>
2016-09-02 23:42:41 -0400

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-condition-clj "0.1.0"
+(defproject com.rentpath/rp-condition-clj "0.1.1-SNAPSHOT"
   :description "Simple condition system for Clojure"
   :url "https://github.com/rentpath/rp-condition-clj"
   :license {:name "Eclipse Public License"

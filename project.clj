912ba00e6848ebc7d0a3bb3b4b112784ddd76709
Jenkins Service <jenkins@jenkins-slave005.tools.useast2.rentpath.com>
2018-05-04 11:09:19 -0400

    Version 0.5.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-condition-clj "0.5.1"
+(defproject com.rentpath/rp-condition-clj "0.5.2-SNAPSHOT"
   :description "Simple condition system for Clojure"
   :url "https://github.com/rentpath/rp-condition-clj"
   :license {:name "Eclipse Public License"
